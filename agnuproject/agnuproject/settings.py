c3ed1be0e4f05f7ac762b147d08c5cd82d3bba66
jeet760 <jeet760@hotmail.com>
2025-12-03 17:35:27 +0530

    changes to settings and wsgi

--- agnuproject/agnuproject/settings.py
+++ agnuproject/agnuproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'agnuproject.agnuapp',
+    'agnuapp',
 ]
 
 MIDDLEWARE = [
